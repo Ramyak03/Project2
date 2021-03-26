178b4531d111a233beac4ea4197790a28bd50640
Ramyak03 <ramya.kosuri20@gmail.com>
2021-03-26 16:38:53 -0500

    Printing in Multiple line using echo -e command

--- 02-print.sh
+++ 02-print.sh
@@ -2,3 +2,8 @@
 #echo Input
 echo  Hello World
 echo welcome to devops training
+
+# To print multiline outputs we need multi line sequences \n
+# Syntax: echo -e "Hello\nWorld"
+
+echo -e "Hello\nWorld"
