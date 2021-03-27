b7e62453a33e184278973806bbac2e6f016fff14
Ramyak03 <ramya.kosuri20@gmail.com>
2021-03-27 13:33:00 -0500

    Learning how to use variables with syntax var=data

---
+++ 03-variables.sh
@@ -0,0 +1,19 @@
+# variable is the name you give to certain set of data
+## without variables
+echo Welcome to Devops training
+echo Training of Devops laste for 70 days
+echo "Devops training covers shell scripting & Ansible"
+
+
+# Declaring a variable , Syntax : var=data
+
+COURSE=Devops
+echo Welcome to ${COURSE} training
+echo Training of ${COURSE}  laste for 70 days
+echo " $COURSE training covers shell scripting & Ansible"
+
+
+
+
+
+
