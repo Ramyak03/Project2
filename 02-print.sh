fc562dd779fa14db969b6a660606c5d1fecabdda
Ramyak03 <ramya.kosuri20@gmail.com>
2021-03-27 12:30:07 -0500

    Printing output in colours

--- 02-print.sh
+++ 02-print.sh
@@ -7,3 +7,16 @@ echo welcome to devops training
 # Syntax: echo -e "Hello\nWorld"
 
 echo -e "Hello\nWorld"
+
+#color code based printing
+#red green yellow blue magenta cyan
+#31  32    33      34   35      36
+# reset 0
+# bold 1
+# syntax: echo -e "\e[COLORCODEmMESSAGE"
+
+echo -e "\e[31mHello in Red"
+echo -e "\e[1;31mHello in BoldRed"
+
+
+
