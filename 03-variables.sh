31812543fee90e0449e556c9d70fa52dd9820cd5
Ramyak03 <ramya.kosuri20@gmail.com>
2021-03-30 11:44:18 -0500

    Property of variables-scalar & arrays

--- 03-variables.sh
+++ 03-variables.sh
@@ -29,22 +29,36 @@ echo Good Morning, Today Date is 2021-03-24
 
 #1.Command Substitution, syntax var=$(commands),Declaring a variable from command output
 DATE=$(date +%F)
-echo Good Morning, Today Date is ${DATE}
+echo Good Morning, Today Date is "${DATE}"
 
 # 2.Arithmetic Substitution ,syntax:var=$((expression))
 ADD=$((2+3))
 echo Addition of 2 +3  is $ADD
 
-#Read Write(default property of any variable)
+#1.Read Write(default property of any variable)
 a=10
 echo $a
 a=20
 echo $a
 
-# if we want to change the property of variables we use readonly
+# 1.if we want to change the property of variables we use readonly
 readonly a
 a=30
 echo $a
+
+# scalar (It can hold only one value)
+a=10
+b=20
+echo $b
+# gives only one value at a time , output is 20.. we can achieve that using myArray[index]=value
+s=( 10 20 30)
+echo "${s[1]}"
+echo "${s[2]}"
+echo "${s[3]}"
+
+
+
+
 
 
 # scalar
